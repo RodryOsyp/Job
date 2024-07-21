94330104b241de35bee47fda6b0c9e4fc9d312ef
TimPlay <osypenkotymur@gmail.com>
2024-07-21 11:40:35 +0300

    jopa

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import header from '/images/bg-header-desktop.svg';
-import Badge from './Components/Badge/Badge';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import header from "/images/bg-header-desktop.svg";
+import Badge from "./Components/Badge/Badge";
 
 function App() {
   const handleBadgeClick = (badge) => {
     console.log(`${badge} clicked`);
   };
-const badges = ["Frontend", "CSS", 'JavaScript']
+  const badges = ["Frontend", "CSS", "JavaScript"];
   return (
     <>
       <div>
-        <img src={header} alt="" className="bg-cover w-full" />
+        <img src={header} alt="" className="bg-cover w-full z-0" />
       </div>
-      <div className="badge-row flex space-x-4 mt-4">
-      {badges.map(e=> <Badge />)}
+      <div className="relative flex justify-center z-10">
+        <div className=" bg-red badge-row m-auto absolute top-[-30px]  flex items-center justify-between w-10/12	shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)] 	">
+          <div className="badge-row flex space-x-4 ">
+            {badges.map((e) => (
+              <Badge text={e} onClick={e} />
+            ))}
+          </div>
+          <button className="text-Cyan text-[15px] hover:cursor-pointer hover:color-red-500	">
+            Clear
+          </button>
+        </div>
       </div>
     </>
   );
--- src/Components/Badge/Badge.jsx
+++ src/Components/Badge/Badge.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Badge = ({ text, onClick }) => {
+const Badge = ({ text = "Error", onClick }) => {
   return (
     <div className="badge-container flex items-center space-x-2 p-2 rounded-lg bg-Grayish_Cyan">
       <div className="text-Cyan text-[15px]">{text}</div>
